649474daa711957d8928f92990542480c008c679
oumayma <selmioumayma89@gmail.com>
2025-03-29 02:08:52 +0100

    Mes modifications

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) {
+        // Vérifier la présence de secrets avant de démarrer
+        if (containsSecrets()) {
+            throw new RuntimeException("🚨 ERREUR : Des secrets ont été détectés ! L'application ne peut pas démarrer.");
+        }
+
         System.out.println("Bienvenue dans l'application DevSecOps !");
     }
+
+    public static boolean containsSecrets() {
+        String dbPassword = "SuperSecret123";
+        String apiKey = "sk_test_51H4hDQqRfLZyFakeKey";
+
+        return dbPassword.contains("SuperSecret123") || apiKey.contains("sk_test_");
+    }
 }
+
